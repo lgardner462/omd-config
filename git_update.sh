2ed04f9d60bbaa743a1003bcf8f33acfd5834b52
OMD site openmind <openmind@om-noc.cm.cluster>
2017-08-30 12:04:05 -0400

    testing123

--- git_update.sh
+++ git_update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-git-sed -f g neuro tsstuff
+git-sed -f g tsstuff tsstuff
 git add . 
 git commit -m "testing123" 
 git push origin openmindtest
-git-sed -f g tsstuff neuro
+git-sed -f g tsstuff tsstuff
 
 
