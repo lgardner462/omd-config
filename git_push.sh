d6209e47a7366e265f8d1c5039e483c3bbaff4bb
OMD site neuro <neuro@om-noc.cm.cluster>
2017-09-06 11:09:40 -0400

    -a

--- git_push.sh
+++ git_push.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-git-sed -f g $(whoami) neuro
+git-sed -f g $(whoami) tsstuff
 git add . 
 git commit -m -a "$@"
 git push origin master
-git-sed -f g neuro $(whoami)
+git-sed -f g tsstuff $(whoami)
 
